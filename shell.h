6e4e4e3746008ee4d047f02267df744f9b3a34cf
Salah Eddine Maarouf <75632264+PRIZZMO@users.noreply.github.com>
2023-04-17 03:18:56 +0000

    Created header file

---
+++ shell.h
@@ -0,0 +1,13 @@
+#ifndef shell_h
+#define shell_h
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <string>
+#include <stdlib.h>
+#include <sys/wait.h>
+
+#define BUFFER_SIZE 1024
+
+#endif
